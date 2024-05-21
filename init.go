e91a20f3e256830914607df92aeeb3b5017c3fe5
cedkeChat01 <123301168+00cedke@users.noreply.github.com>
2024-05-21 19:31:38 +0200

    Update init.go

--- init.go
+++ init.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+    "log"
+    "net/http"
+)
+
 func main() {
-	server := newServer()
-	server.Start()
+    setupRoutes()
+
+    log.Println("Server started on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
